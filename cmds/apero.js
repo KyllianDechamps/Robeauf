932e1764b4baaf4fff9b47d1ae06daafeebc0c44
Kyllian Dechamps <kylliandechamps@gmail.com>
2020-04-26 00:28:11 +0200

    added command apero?

---
+++ cmds/apero.js
@@ -0,0 +1,7 @@
+module.exports.run = async (bot , message , args) => {
+    message.reply("C'est l'heure de l'apéro, et si ça l'est pas, c'est bien TOUJOURS l'heure de l'apéro quelque part")
+}
+
+module.exports.help = {
+    name : "apero?"
+}
