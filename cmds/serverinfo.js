b26bd9313f9848b7b1bebc0e4dcea7a297872d9c
Kyllian Dechamps <la172051@student.helha.be>
2020-04-26 02:35:40 +0200

    Checkpoint

---
+++ cmds/serverinfo.js
@@ -0,0 +1,35 @@
+module.exports.run = async (bot , message , args) => {
+    var server = message.guild
+    console.log(server)
+
+
+    const embed = {
+        color: 0x90ff00,
+        description: server.id,
+        title: 'Info du serveur',
+        image: server.icon,
+        fields: [
+            {
+                name: 'Uptime',
+                value: 2,
+            },
+            {
+                name: 'Nombre de serveurs',
+                value: bot.guilds.cache.array().length,
+                inline: true,
+            },
+            {
+                name: 'Nombre de beaufs',
+                value: server.memberCount,
+                inline: true,
+            }
+        ],
+        timestamp: new Date()
+    };
+
+    await message.channel.send({embed : embed})
+}
+
+module.exports.help = {
+    name : "serverinfo"
+}
