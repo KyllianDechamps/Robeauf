e9b1ecfe25d2d54a767382770649f15f533ccf4f
Mealin <la172051@student.helha.be>
2020-04-29 13:53:12 +0200

    Code cleaned

--- cmds/marcel.js
+++ cmds/marcel.js
@@ -2,16 +2,10 @@ const fs = require('fs')
 module.exports.run = async (bot , message , args) => {
     //Lecture du dossier contenant les images de Marcel
     fs.readdir('./images/marcel',  (err,files) => {
-
         if (err) console.log(err)
-
         let nbimages = files.length
         let random = Math.floor(Math.random() * nbimages)
-
         message.channel.send({files: ["./images/marcel/"+files[random]]})
-
-
-
     })
 }
 
