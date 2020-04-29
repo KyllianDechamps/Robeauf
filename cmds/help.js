315cf05632f9733bb9a43894aa8d55536d0018f1
Mealin <la172051@student.helha.be>
2020-04-30 00:25:16 +0200

    Checkpoint 30/04 0h

--- cmds/help.js
+++ cmds/help.js
@@ -1,8 +1,16 @@
-const fs = require('fs')
+const fs = require('fs') , discord = require('discord.js')
 module.exports.run = async (bot , message , args) => {
-    bot.commands.forEach((f,i) =>{
-        console.log(f.help.info)
+
+
+
+
+    var embed = new discord.MessageEmbed();
+    await bot.commands.forEach((f,i) =>{
+        embed.addField(f.help.name,f.help.info)
     })
+    embed.setColor([255,0,255])
+
+    await message.channel.send(embed)
 }
 
 module.exports.help = {
