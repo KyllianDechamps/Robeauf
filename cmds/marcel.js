f871e18fbee6e32a2acb1fa7bb8b5fae61c6ba56
Mealin <la172051@student.helha.be>
2020-04-29 13:51:22 +0200

    marcel command finished

--- cmds/marcel.js
+++ cmds/marcel.js
@@ -1,5 +1,18 @@
+const fs = require('fs')
 module.exports.run = async (bot , message , args) => {
-    //Ce que ma commande doit faire
+    //Lecture du dossier contenant les images de Marcel
+    fs.readdir('./images/marcel',  (err,files) => {
+
+        if (err) console.log(err)
+
+        let nbimages = files.length
+        let random = Math.floor(Math.random() * nbimages)
+
+        message.channel.send({files: ["./images/marcel/"+files[random]]})
+
+
+
+    })
 }
 
 module.exports.help = {
