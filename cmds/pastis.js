67b10dba0ac54ab99d69e354f4d441c5f2c7cf81
Mealin <la172051@student.helha.be>
2020-04-29 14:09:39 +0200

    pastis command finished

--- cmds/pastis.js
+++ cmds/pastis.js
@@ -1,5 +1,12 @@
+const fs = require('fs')
 module.exports.run = async (bot , message , args) => {
-    //Ce que ma commande doit faire
+    //Lecture du dossier contenant les images de Pastis
+    fs.readdir('./images/pastis',  (err,files) => {
+        if (err) console.log(err)
+        let nbimages = files.length
+        let random = Math.floor(Math.random() * nbimages)
+        message.channel.send({files: ["./images/pastis/"+files[random]]})
+    })
 }
 
 module.exports.help = {
