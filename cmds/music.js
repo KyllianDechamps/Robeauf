ffb4ce82051347c1d8061a71815e907676891959
Mealin <la172051@student.helha.be>
2020-05-01 17:16:17 +0200

    Code cleaned

--- cmds/music.js
+++ cmds/music.js
@@ -17,12 +17,10 @@ module.exports.run = async (bot , message , args,opt) => {
 
     //Recup infos
     let info = await ytdl.getInfo(args[0])
-
     let data = opt.active.get(message.guild.id) || {}
     if(!data.connection) data.connection = await voiceChannel.join()
     if (!data.queue) data.queue = []
     data.guildID = message.guild.id
-
     //Info queue
     data.queue.push({
         songTitle: info.title,
@@ -30,49 +28,29 @@ module.exports.run = async (bot , message , args,opt) => {
         url: args[0],
         annonceChannel : message.channel.id
     })
-
-
     //Dispatcher check
-
     if(!data.dispatcher) await play(bot,opt,data)
     else{
-        message.channel.send(`Added to queue: ${info.title} | Requested by ${message.author.id}`)
+        message.channel.send(`Added to queue: ${info.title} | Requested by ${message.author.tag}`)
     }
-
     //Map update
     opt.active.set(message.guild.id , data)
-
-
-
-
 }
 
-module.exports.help = {
-    name : "music",
-    info : "Envoyez un lien youtube"
-}
-
+//Functions
 async function play(client,opt,data) {
-
-
     //Now playing
     client.channels.fetch(data.queue[0].annonceChannel).then(channel => channel.send(`Now playing : ${data.queue[0].songTitle} | Requested by  ${data.queue[0].requester}`))
     //Update dispatcher
     data.dispatcher = await data.connection.play(ytdl(data.queue[0].url ))
-    console.log(data.queue[0].url)
     data.dispatcher.guildID = data.guildID
-
     data.dispatcher.once('finish',function (){
         finish(client,opt,data.dispatcher)
-        console.log("Fini")
     })
 }
 
 function finish(client,opt,dispatcher) {
     let fetched = opt.active.get(dispatcher.guildID)
-    console.log(opt.active)
-    console.log(dispatcher.guildID)
-    console.log(fetched)
     fetched.queue.shift()
     //Check if queue empty
     if (fetched.queue.length > 0){
@@ -84,4 +62,9 @@ function finish(client,opt,dispatcher) {
         if (vc) vc.leave()
     }
 
+}
+
+module.exports.help = {
+    name : "music",
+    info : "Envoyez un lien youtube"
 }
