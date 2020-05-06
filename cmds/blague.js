77511fd0599cdefc449282935a965406b2c1159c
Mealin <la172051@student.helha.be>
2020-05-07 00:03:31 +0200

    Create blague.js

---
+++ cmds/blague.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js')
+const http = require('http')
+module.exports.run = async (bot , message , args , opt) => {
+
+    let data =''
+    http.get('http://api.kd-dev.be/beauf',(res)=>{
+        const {statusCode}= res
+
+        if (statusCode !== 200){
+            message.channel.send("Je ne peux pas fournir de blague pour le moment . Code erreur "+statusCode)
+        }else{
+            res.on('data', (chunk) => {
+                data+=chunk
+            })
+            res.on('end',()=>{
+                const obj = JSON.parse(data);
+                const min = 0, max = obj.length;
+                const rand = Math.floor(Math.random() * (max - min) + min);
+                let embed = new Discord.MessageEmbed()
+                embed.setColor("RANDOM")
+                embed.addField("Test", obj[rand].blague)
+
+                console.log(obj)
+                message.channel.send(embed)
+            })
+        }
+
+    })
+
+}
+
+module.exports.help = {
+    name : "blague",
+    info : "Envoie une blague grasse"
+}
