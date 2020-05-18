e572a558aa0c123ef7dd17000036ee73cc4778b4
Mealin <la172051@student.helha.be>
2020-05-18 13:33:40 +0200

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -3,17 +3,31 @@ module.exports.run = async (bot , message , args) => {
 
 
 
+if (args[0] && bot.commands.find(f => f.help.name == args[0])){
 
+    var command = bot.commands.get(args[0])
+    var embed = new discord.MessageEmbed()
+    embed.setTitle(command.help.name)
+    embed.setColor("RANDOM")
+    embed.addField("Info", command.help.info , true)
+    embed.addField("Usage", process.env.PREFIX + command.help.usage , true)
+
+    message.channel.send(embed)
+
+}else{
     var embed = new discord.MessageEmbed();
     await bot.commands.forEach((f,i) =>{
         embed.addField(f.help.name,f.help.info)
     })
-    embed.setColor([255,0,255])
+    embed.setColor("RANDOM")
 
     await message.channel.send(embed)
 }
 
+}
+
 module.exports.help = {
     name : "help",
-    info : "Commande d'aide"
+    info : "Commande d'aide",
+    usage : "help [command name]"
 }
