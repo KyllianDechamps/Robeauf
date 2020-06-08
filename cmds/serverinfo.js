11f6caa1c1e47fa99f534706393a35138b6c7c47
Mealin <la172051@student.helha.be>
2020-06-08 22:53:18 +0200

    Update serverinfo.js

--- cmds/serverinfo.js
+++ cmds/serverinfo.js
@@ -1,17 +1,17 @@
 module.exports.run = async (bot , message , args) => {
     var server = message.guild
-    console.log(server)
+
 
 
     const embed = {
         color: 0x90ff00,
-        description: server.id,
+        description: "Server ID " +server.id,
         title: 'Info du serveur',
         image: server.icon,
         fields: [
             {
                 name: 'Uptime',
-                value: 2,
+                value: bot.uptime,
             },
             {
                 name: 'Nombre de serveurs',
