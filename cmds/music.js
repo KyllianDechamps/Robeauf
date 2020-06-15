dce6fb3788694a780b729dd53661277cdba4c6a8
Mealin <la172051@student.helha.be>
2020-06-15 10:43:54 +0200

    Update music.js

--- cmds/music.js
+++ cmds/music.js
@@ -1,70 +1,31 @@
-const ytdl = require('ytdl-core')
 module.exports.run = async (bot , message , args,opt) => {
 
-    //Check le canal vocal (Si l'auteur est bien dedans + si le bot a les permissions)
-    const voiceChannel = message.member.voice.channel;
-    if (!voiceChannel)
-        return message.channel.send(
-            "You need to be in a voice channel to play music!"
-        );
-    const permissions = voiceChannel.permissionsFor(message.client.user);
-    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-        return message.channel.send(
-            "I need the permissions to join and speak in your voice channel!"
-        );
-    }
+    if(!message.member.voice.channel) return message.channel.send(`**You're not in a voice channel**`);
 
+    if (!args[0]) return message.channel.send(`**Please enter a music**`);
 
-    //Recup infos
-    let info = await ytdl.getInfo(args[0])
-    let data = opt.active.get(message.guild.id) || {}
-    if(!data.connection) data.connection = await voiceChannel.join()
-    if (!data.queue) data.queue = []
-    data.guildID = message.guild.id
-    //Info queue
-    data.queue.push({
-        songTitle: info.title,
-        requester: message.author.tag,
-        url: args[0],
-        annonceChannel : message.channel.id
-    })
-    //Dispatcher check
-    if(!data.dispatcher) await play(bot,opt,data)
-    else{
-        message.channel.send(`Added to queue: ${info.title} | Requested by ${message.author.tag}`)
-    }
-    //Map update
-    opt.active.set(message.guild.id , data)
-}
+    const aSongIsAlreadyPlaying = opt.player.isPlaying(message.guild.id);
 
-//Functions
-async function play(client,opt,data) {
-    //Now playing
-    client.channels.fetch(data.queue[0].annonceChannel).then(channel => channel.send(`Now playing : ${data.queue[0].songTitle} | Requested by  ${data.queue[0].requester}`))
-    //Update dispatcher
-    data.dispatcher = await data.connection.play(ytdl(data.queue[0].url ))
-    data.dispatcher.guildID = data.guildID
-    data.dispatcher.once('finish',function (){
-        finish(client,opt,data.dispatcher)
-    })
-}
-
-function finish(client,opt,dispatcher) {
-    let fetched = opt.active.get(dispatcher.guildID)
-    fetched.queue.shift()
-    //Check if queue empty
-    if (fetched.queue.length > 0){
-        opt.active.set(dispatcher.guildID,fetched)
-        play(client,opt,fetched)
-    }else{
-        opt.active.delete(dispatcher.guildID)
-        let vc = fetched.connection.channel
-        if (vc) vc.leave()
+    // If there's already a song playing
+    if(aSongIsAlreadyPlaying){
+        // Add the song to the queue
+        const song = await opt.player.addToQueue(message.guild.id, args.join(" "),message.author).catch(err => {
+            console.error(err)
+        });
+        message.channel.send(`**Song ${song.name} added to queue**`);
+    } else {
+        // Else, play the song
+        const song = await opt.player.play(message.member.voice.channel, args.join(" "),message.author).catch(err => {
+            console.error(err)
+        });
+        message.channel.send(`**Currently playing ${song.name}**`);
     }
 
 }
 
 module.exports.help = {
     name : "music",
-    info : "Envoyez un lien youtube"
+    info : "Envoyez un lien youtube",
+    disabled : true,
+    usage : "music LienYoutube"
 }
