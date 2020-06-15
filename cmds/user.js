a907cb1a2faab86d1ee9881c63dcc429c7c1baed
Mealin <la172051@student.helha.be>
2020-06-15 13:12:56 +0200

    Create user.js

---
+++ cmds/user.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js')
+module.exports.run = async (bot , message , args , opt) => {
+    let user = message.mentions.users.first()
+
+    let embed = new Discord.MessageEmbed()
+    embed.setImage(user.displayAvatarURL())
+    embed.setTitle(user.tag)
+    embed.addField('ID',user.id)
+    embed.addField('Créé le',user.createdAt)
+    embed.setColor("LUMINOUS_VIVID_PINK")
+    message.channel.send(embed)
+}
+
+module.exports.help = {
+    name : "user",
+    info : "Donne les infos d'un utilisateur du serveur",
+    usage : "user [mention ou ID]"
+}
